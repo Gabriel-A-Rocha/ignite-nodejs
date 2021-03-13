9a233a441754d57dbf47437124f25276690de3bb
Gabriel Rocha <gabriel.altair.rocha@gmail.com>
2021-03-12 22:30:56 -0500

    feat: include start script with nodemon

--- src/index.js
+++ src/index.js
@@ -5,6 +5,4 @@ const app = express();
 const port = 3333;
 app.listen(port, () => console.log(`Server running at port ${port}`));
 
-app.get("/", (req, res) => {
-  return res.json({ msg: "OK" });
-});
+app.get("/", (req, res) => res.json({ msg: "OK" }));
