ea5defd5b7a8cf8071046a20e42a4cd16979e42b
Gabriel Rocha <gabriel.altair.rocha@gmail.com>
2021-03-12 23:08:20 -0500

    feat: add json body parser

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
+const { json } = require("express");
 const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 const port = 3333;
 app.listen(port, () => console.log(`Server running at port ${port}`));
 
-app.get("/", (req, res) => res.json({ msg: "OK" }));
+app.get("/courses", (req, res) =>
+  res.json(["course 1", "course 2", "course 3"])
+);
+
+app.put("/courses/:id", (req, res) => {
+  const { id } = req.params;
+  const { token } = req.query;
+
+  return res.json({ id, token });
+});
+
+app.post("/courses", (req, res) => {
+  const obj = req.body;
+  console.log("🚀 ~ file: index.js ~ line 21 ~ app.post ~ obj", obj);
+
+  return res.json(obj);
+});
